090e1738fc3cf4f958e12c9601423995366ee00f
Yulian-Caesar <yulik19971997@gmail.com>
2019-09-16 18:18:47 +0300

    add order -section

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,6 +1,16 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
 let menuToggle = function () {
     $(".menu-mobile").toggleClass("open");
     $(".header__menu-toggle").toggleClass("open");
 }
 
 $(document).on('click', '.menu-toggle', menuToggle);
+$(document).on('click', '.menu-mobile__button', menuToggle);
+
--- app/components/inquirer/inquirer.js
+++ app/components/inquirer/inquirer.js
@@ -1,24 +1,27 @@
-function progressBar () {
-    const numberSteps = $(".inquirer-item").length;
-    let currentIndex = Number($(".inquirer-item.current").attr("data-question"));
-    const percentage = (currentIndex * 100)/ numberSteps;
-    $(".inquirer-progress__inner").width(percentage+ "%");
-}
-
-let toggleInquirerCurrent = function () {
-    $(".inquirer-item.current").next().addClass("current");
-    $(".inquirer-item.current").prev().removeClass("current");
-    if ($(".inquirer-item:last-child").hasClass("current")) {
-        $(".inquirer-box2").addClass('hide');
-        $(".inquirer-box1").addClass('active');
-    }
-};
-
-$(document).on("click", ".inquirer-test__button", toggleInquirerCurrent);
-$(document).on("click", ".inquirer-test__button", progressBar);
-
 $(document).ready(function () {
     progressBar();
+    function progressBar () {
+        let numberSteps = $(".inquirer-item").length;
+        let currentIndex = Number($(".inquirer-item.current").attr("data-question"));
+        const percentage = (currentIndex * 100)/ numberSteps;
+        $(".inquirer-progress__inner").width(percentage+ "%");
+        $(".js--progress__current").html(currentIndex);
+        $(".js--progress__all").html(numberSteps);
+    }
+
+    let toggleInquirerCurrent = function () {
+        let numberSteps = $(".inquirer-item").length;
+        let currentIndex = Number($(".inquirer-item.current").attr("data-question"));
+        $(".inquirer-item.current").next().addClass("current");
+        $(".inquirer-item.current").prev().removeClass("current");
+        if (numberSteps == currentIndex) {
+            $(".inquirer-box2").addClass('hide');
+            $(".inquirer-box1").addClass('show');
+        }
+    };
+
+    $(document).on("click", ".inquirer-test__button", toggleInquirerCurrent);
+    $(document).on("click", ".inquirer-test__button", progressBar);
 });
 
 
