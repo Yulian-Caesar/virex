e896daae8ca0b001a9795af113dfef346b71f51e
chaplygineverad <s.chaplygin@everad.com>
2019-09-17 17:27:23 +0300

    section .myths completed

--- app/components/myths/myths.js
+++ app/components/myths/myths.js
@@ -1,10 +1,18 @@
+/*
 const buttons = document.querySelectorAll(".myths__button-show");
 
 for ( let i = 0; i < buttons.length; i++) {
 
     buttons[i].onclick = function () {
         buttons[i].classList.toggle("-minus");
-        this.parentElement.classList.toggle("open-answer");
+        this.parentElement.classList.toggle("-open-answer");
     }
 
-}
+}*/
+
+let mythsToggle = function () {
+    $(this).parents(".myths__item-box").toggleClass("-open-answer");
+    $(this).toggleClass("-minus");
+};
+
+$(document).on("click", ".myths__button-show", mythsToggle);
